b9a74538db976af9b12a0f630b8bb62b83d970b0
samwoodson <samuelwoodson@gmail.com>
2016-05-31 16:00:23 -0700

    added supervised portal doors

--- lib/portal.ex
+++ lib/portal.ex
@@ -7,13 +7,12 @@ defmodule Portal do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(Portal.Worker, [arg1, arg2, arg3]),
+      worker(Portal.Door, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
+    opts = [strategy: :simple_one_for_one, name: Portal.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
@@ -34,6 +33,10 @@ defmodule Portal do
     end
 
     portal
+  end
+
+  def shoot(color) do
+    Supervisor.start_child(Portal.Supervisor, [color])
   end
 
 end
