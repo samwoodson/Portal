7e0971648f10dfb8b9eeffacbb369fcc0416ac49
samwoodson <samuelwoodson@gmail.com>
2016-05-31 17:38:19 -0700

    added ExUnit tests

--- test/portal_test.exs
+++ test/portal_test.exs
@@ -1,8 +1,35 @@
 defmodule PortalTest do
   use ExUnit.Case
-  doctest Portal
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "shoot: starts a Portal.Door with the given color" do
+    Portal.shoot(:orange)
+    assert Portal.Door.get(:orange)
+  end
+
+  test "transfer: push data into left struct" do
+    Portal.shoot(:brown)
+    Portal.shoot(:green)
+    Portal.transfer(:brown, :green, [1])
+    assert [1] == Portal.Door.get(:brown)
+    assert [] == Portal.Door.get(:green)
+  end
+
+  test "push_right: push head of left into right struct" do
+    Portal.shoot(:orange)
+    Portal.shoot(:blue)
+    portal = Portal.transfer(:orange, :blue, [1])
+    Portal.push_right(portal)
+    assert [] == Portal.Door.get(:orange)
+    assert [1] == Portal.Door.get(:blue)
+  end
+
+  test "push_left: push head of right into left struct" do
+    Portal.shoot(:yellow)
+    Portal.shoot(:magenta)
+    portal = Portal.transfer(:yellow, :magenta, [])
+    Portal.Door.push(:magenta, 3)
+    Portal.push_left(portal)
+    assert [3] == Portal.Door.get(:yellow)
+    assert [] == Portal.Door.get(:magenta)
   end
 end
